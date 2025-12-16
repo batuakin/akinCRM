db4237ffe705879f2891333029d36fc7f32c451b
batuakin <batuhanakin71@gmail.com>
2025-12-16 20:27:13 +0300

    Initial commit

---
+++ src/main/java/com/batuakin/akinCRM/AkinCrmApplication.java
@@ -0,0 +1,13 @@
+package com.batuakin.akinCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkinCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkinCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batuakin/akinCRM/AkinCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batuakin.akinCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkinCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
